ebadde51daae46391d6d18bb5724193be6aa850d
YFZ-Q <71964658+YFZ-Q@users.noreply.github.com>
2021-08-23 14:49:39 +0800

    Add files via upload

---
+++ poe/src/tests.rs
@@ -0,0 +1,75 @@
+use crate::{mock::*, Error};
+use frame_support::{assert_noop, assert_ok};
+use super::*;
+
+#[test]
+fn create_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim = vec![0,1];
+		assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+		assert_eq!(
+			Proofs::<Test>::get(&claim), 
+			Some((1, frame_system::Pallet::<Test>::block_number())
+        ));
+	})
+}
+
+#[test]
+fn create_claim_failed_when_claim_already_exist(){
+	new_test_ext().execute_with(|| {
+		let claim = vec![0,1];
+		let _ = (PoeModule::create_claim(Origin::signed(1), claim.clone()));
+        assert_noop!(
+            PoeModule::create_claim(Origin::signed(1), claim.clone()),
+            Error::<Test>::ProofAlreadyExist
+	    );
+	})
+}
+
+#[test]
+fn revoke_claim_works(){
+	new_test_ext().execute_with(||{
+		let claim = vec![0,1];
+		let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+		assert_ok!(PoeModule::revoke_claim(Origin::signed(1), claim.clone()));
+		assert_eq!(Proofs::<Test>::get(&claim),None);
+	})
+}
+
+#[test]
+fn revoke_claim_failed_when_claim_is_not_exist(){
+	new_test_ext().execute_with(|| {
+		let claim = vec![0,1];
+		
+        assert_noop!(
+            PoeModule::revoke_claim(Origin::signed(1), claim.clone()),
+            Error::<Test>::ClaimNotExist
+        );
+	})
+}
+
+#[test]
+fn transfer_claim_works() {
+	new_test_ext().execute_with(|| {
+		let claim:Vec<u8> = vec![0,1];
+
+        let _ = PoeModule::create_claim(Origin::signed(1),claim.clone());
+
+		assert_ok!(PoeModule::transfer_claim(Origin::signed(1), claim.clone(), 2));
+		                                                   
+	});
+}
+
+#[test]
+fn transfer_claim_failed() {
+	new_test_ext().execute_with(||{
+		let claim = vec![0,1];
+		
+		PoeModule::create_claim(Origin::signed(1),claim.clone());
+	
+		assert_noop!(
+			PoeModule::transfer_claim(Origin::signed(1),claim.clone(),2),
+			Error::<Test>::NotClaimOwner
+		);
+	})
+}
